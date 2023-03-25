a73802d095c2182d40e76da2a0254565470d3e5c
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-03-25 00:05:49 -0300

    Change copy and remove old files

--- dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-inotifywait -m posts/ -m public/ -m pages/ -m index.css -m tailwind.config.js -e close_write |
-    while read; do
-        tailwindcss -i index.css -o _dist/index.css && ruby build.rb
-    done
